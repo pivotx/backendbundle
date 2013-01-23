01f3a59ad3626dc65198b54bf4a7baa69fbe6949
Marcel Wouters <marcel@twokings.nl>
2013-01-23 12:42:04 +0100

    Renamed back.

--- PivotX/BackendBundle/BackendBundle.php
+++ PivotX/BackendBundle/BackendBundle.php
@@ -5,18 +5,23 @@ namespace PivotX\BackendBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class PivotXBackendBundle extends Bundle
+class BackendBundle extends Bundle
 {
     public function boot()
     {
         try {
             $service = $this->container->get('pivotx.routing');
 
+            echo "HIER<br/>\n";
+
             $fname = dirname(dirname(dirname(__FILE__))).'/Resources/config/pivotxrouting.yml';
+            echo 'filename['.$fname.']<br/>';
+            die('');
             $service->load($fname);
         }
         catch (\InvalidArgumentException $e) {
         }
+        echo 'hier wel';
 
         // force loading of our views
         // @todo this is not pretty
